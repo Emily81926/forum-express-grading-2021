e7426dea304e73d5d3e323d212728417fbefff49
Emily81926 <emily81926@gmail.com>
2021-11-07 18:59:31 +0800

    Revise app.js

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
-const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 app.use(flash())
 app.use('/upload', express.static(__dirname + '/upload'))
 
@@ -28,8 +28,6 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
-app.use(methodOverride('_method'))
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
