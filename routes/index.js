a8358048e25172ce27170a3bb97e949530f202a6
Emily81926 <emily81926@gmail.com>
2021-11-07 17:07:20 +0800

    heroku PG switch to ClearDB mysql

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ module.exports = (app, passport) => {
   app.get('/restaurants', authenticated,restController.getRestaurants)
 
   app.get('/admin', authenticatedAdmin,(req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', authenticatedAdmin,adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
