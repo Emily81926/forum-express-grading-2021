4a20522a293809593e4e646e4502b3d986bedede
Emily81926 <emily81926@gmail.com>
2021-11-23 21:56:05 +0800

    refactor: POST /api/admin/restaurants

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.engine('handlebars', handlebars({
   helpers: require('./config/handlebars-helpers')}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
--- services/adminService.js
+++ services/adminService.js
@@ -27,6 +27,43 @@ const adminService = {
           })
       })
   },
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      callback({ status: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          return callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+      })
+    }
+    else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null,
+        CategoryId: req.body.categoryId
+      }).then((restaurant) => {
+        callback({ status: 'success', message: 'restaurant was successfully created' })
+      })
+    }
+  },
 }
 
 module.exports = adminService
